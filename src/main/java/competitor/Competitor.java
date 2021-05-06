3cc759ce04615cc83b011272aa4ce5369a3b53a7
Sudden123 <emil.sundius@gmail.com>
2021-05-06 15:17:34 +0200

    ...

--- src/main/java/competitor/Competitor.java
+++ src/main/java/competitor/Competitor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Competitor {
 
     public String getFirstName(String input) {
+
         System.out.println("Type your first name: ");
         Scanner scan = new Scanner(System.in);
         String name = scan.nextLine();
         return name;
+
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
-module org.example {
+module Grupp5.Project {
+
     requires javafx.controls;
     requires javafx.fxml;
-
     opens org.example to javafx.fxml;
     exports org.example;
 }
