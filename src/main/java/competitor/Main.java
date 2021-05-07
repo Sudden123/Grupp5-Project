cd7fc8627dfc8a0935dd573bbcafcf2c2afa671f
Sudden123 <emil.sundius@gmail.com>
2021-05-07 09:53:47 +0200

    Lite kod

---
+++ src/main/java/competitor/Main.java
@@ -0,0 +1,27 @@
+package competitor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Type your first name: ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+
+        Competitor comp = new Competitor();
+        comp.setFirstName(name);
+        String firstName = comp.getFirstName(name);
+        System.out.println("Type your last name: ");
+
+        String name2 = scan.nextLine();
+        comp.setLastName(name2);
+        String lastName = comp.getLastName(name2);
+
+        System.out.println(firstName + " " + lastName);
+
+    }
+
+
+}
+
+
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module Grupp5.Project {
-
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens org.example to javafx.fxml;
-    exports org.example;
-}
