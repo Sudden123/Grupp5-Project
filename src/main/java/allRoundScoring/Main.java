b4d22202481946a494e2ea3364eec90457c823cd
AnnaMakuch <a.makuchova@gmail.com>
2021-05-10 16:40:12 +0200

    polishing

--- src/main/java/allRoundScoring/Main.java
+++ src/main/java/allRoundScoring/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Choose action: ");
-        System.out.println("1. Register new competitor: ");
-        System.out.println("2. Load old competitor: ");
-        System.out.println("3. Show results in excel: ");
+        System.out.println("Choose action (type a number and press enter): ");
+        System.out.println("1. Register new competitor");
+        System.out.println("2. Load en existing competitor");
+        System.out.println("3. Show score in excel");
 
         int operator = scan.nextInt();
 
@@ -16,42 +16,40 @@ public class Main {
             case 1:
                 System.out.println("Choose event: \n1. Decathlon\n2. Heptathlon");
                 int event = scan.nextInt();
-                if(event == 1){
-                    System.out.println("Choose field event: \n1. 100 m: \n2. Long jump: \n 3.Shot put: \n4. High jump: \n5. 400 m: \n6. 110 m Hurdles: \n7. Discus throw: \n8. Pole vault: \n9. Javelin throw: \n10. 1500 m: ");
-                    int field = scan.nextInt();
-                    Performance value = new Performance();
-                    double perfValue = scan.nextDouble();
-                    value.setPerformanceValue(perfValue);
-                    double finalValue = value.getPerformanceValue(perfValue);
-                    System.out.println("The saved value is " + finalValue);
-                }else if(event == 2){
-                    System.out.println("Choose field event: \n1. 100 m Hurdles: \n2. High jump: \n 3.Shot put: \n4. 200 m: \n5. Long jump: \n6. Javelin throw: \n7. 800 m: ");
-                    int field = scan.nextInt();
-                }
 
-                System.out.println("Type your first name: ");
-
-                String name = scan.nextLine();
-
+                System.out.println("Type competitor's first name: ");
                 Competitor comp = new Competitor();
+                String name = scan.nextLine();
                 comp.setFirstName(name);
                 String firstName = comp.getFirstName(name);
-                System.out.println("Type your last name: ");
 
+                System.out.println("Type competitor's last name: ");
                 String name2 = scan.nextLine();
                 comp.setLastName(name2);
                 String lastName = comp.getLastName(name2);
+                System.out.println("Competitor" + firstName + " " + lastName + " has been successfully registered." + "\n");
 
-                System.out.println(firstName + " " + lastName);
+                if (event == 1) {
+                    System.out.println("Choose field event: \n1. 100 m: \n2. Long jump: \n3. Shot put: \n4. High jump: \n5. 400 m: \n6. 110 m Hurdles: \n7. Discus throw: \n8. Pole vault: \n9. Javelin throw: \n10. 1500 m: ");
+                    int field = scan.nextInt();
+
+                    Performance value = new Performance();
+                    System.out.println("Enter a performance value: ");
+                    double perfValue = scan.nextDouble();
+                    value.setPerformanceValue(perfValue);
+                    double finalValue = value.getPerformanceValue(perfValue);
+                    System.out.println("The saved value for 100 m is " + finalValue);
+                    break;
+
+                } else if (event == 2) {
+                    System.out.println("Choose field event: \n1. 100 m Hurdles: \n2. High jump: \n 3.Shot put: \n4. 200 m: \n5. Long jump: \n6. Javelin throw: \n7. 800 m: ");
+                    int field = scan.nextInt();
+                }
                 break;
 
             case 2:
 
         }
-
-
-
-
 
 
     }
