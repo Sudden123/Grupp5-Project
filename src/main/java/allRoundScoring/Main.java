b03a658704b6af8995a8acc756841877c4e716c7
AnnaMakuch <a.makuchova@gmail.com>
2021-05-10 19:00:39 +0200

    computing score for 100m

--- src/main/java/allRoundScoring/Main.java
+++ src/main/java/allRoundScoring/Main.java
@@ -16,6 +16,7 @@ public class Main {
             case 1:
                 System.out.println("Choose event: \n1. Decathlon\n2. Heptathlon");
                 int event = scan.nextInt();
+                scan.nextLine();
 
                 System.out.println("Type competitor's first name: ");
                 Competitor comp = new Competitor();
@@ -27,19 +28,25 @@ public class Main {
                 String name2 = scan.nextLine();
                 comp.setLastName(name2);
                 String lastName = comp.getLastName(name2);
-                System.out.println("Competitor" + firstName + " " + lastName + " has been successfully registered." + "\n");
+                System.out.println("Competitor " + firstName + " " + lastName + " has been successfully registered." + "\n");
 
                 if (event == 1) {
                     System.out.println("Choose field event: \n1. 100 m: \n2. Long jump: \n3. Shot put: \n4. High jump: \n5. 400 m: \n6. 110 m Hurdles: \n7. Discus throw: \n8. Pole vault: \n9. Javelin throw: \n10. 1500 m: ");
                     int field = scan.nextInt();
 
-                    Performance value = new Performance();
                     System.out.println("Enter a performance value: ");
-                    double perfValue = scan.nextDouble();
-                    value.setPerformanceValue(perfValue);
-                    double finalValue = value.getPerformanceValue(perfValue);
-                    System.out.println("The saved value for 100 m is " + finalValue);
-                    break;
+
+                    switch(field){
+                        case 1:
+                            Performance value = new Performance();
+                            double perfValue = scan.nextDouble();
+                            value.setPerformanceValue(perfValue);
+                            //double finalValue = value.getPerformanceValue();
+                            System.out.println("The saved value for 100 m is " + perfValue);
+                            System.out.println("The score is " +  value.computeScore(field));
+                            break;
+                    }
+
 
                 } else if (event == 2) {
                     System.out.println("Choose field event: \n1. 100 m Hurdles: \n2. High jump: \n 3.Shot put: \n4. 200 m: \n5. Long jump: \n6. Javelin throw: \n7. 800 m: ");
--- src/main/java/allRoundScoring/Performance.java
+++ src/main/java/allRoundScoring/Performance.java
@@ -2,7 +2,7 @@ package allRoundScoring;
 
 public class Performance {
 
-    private double performanceValue = 0;
+    private double performanceValue=0;
 
 
     public void setPerformanceValue(double input) {
@@ -10,8 +10,21 @@ public class Performance {
         performanceValue = input;
     }
 
-    public double getPerformanceValue(double input) {
+    public double getPerformanceValue() {
 
         return performanceValue;
     }
+
+    public int computeScore(int input){
+
+        int score= 0;
+        switch(input){
+            case 1:
+                double helpScore= Math.pow((18- performanceValue),1.81);
+                score= (int) (25.4347*helpScore);
+                break;
+        }
+        return score;
+    }
+
 }
