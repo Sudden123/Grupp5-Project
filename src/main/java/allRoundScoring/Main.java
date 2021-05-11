28d1f4c83cd5968a0f5062bfe46d144dbb12aff5
Sudden123 <emil.sundius@gmail.com>
2021-05-11 10:19:40 +0200

    Added units to performance value

--- src/main/java/allRoundScoring/Main.java
+++ src/main/java/allRoundScoring/Main.java
@@ -34,18 +34,26 @@ public class Main {
                     System.out.println("Choose field event: \n1. 100 m: \n2. Long jump: \n3. Shot put: \n4. High jump: \n5. 400 m: \n6. 110 m Hurdles: \n7. Discus throw: \n8. Pole vault: \n9. Javelin throw: \n10. 1500 m: ");
                     int field = scan.nextInt();
 
-                    System.out.println("Enter a performance value: ");
 
+
+                    Performance value = new Performance();
                     switch(field){
                         case 1:
-                            Performance value = new Performance();
+                            System.out.println("Enter a performance value in seconds: ");
                             double perfValue = scan.nextDouble();
                             value.setPerformanceValue(perfValue);
                             //double finalValue = value.getPerformanceValue();
-                            System.out.println("The saved value for 100 m is " + perfValue);
+                            System.out.println("The saved value for 100 m is " + perfValue + "s");
                             System.out.println("The score is " +  value.computeScore(field));
                             break;
 
+                        case 2:
+                            System.out.println("Enter a performance value in meters: ");
+                            double longJump = scan.nextDouble();
+                            value.setPerformanceValue(longJump);
+                            System.out.println("The saved value for Long jump is " + longJump + "m");
+                            System.out.println("The score is " +  value.computeScore(field));
+                            break;
                     }
 
 
@@ -53,24 +61,30 @@ public class Main {
                     System.out.println("Choose field event: \n1. 100 m Hurdles: \n2. High jump: \n3. Shot put: \n4. 200 m: \n5. Long jump: \n6. Javelin throw: \n7. 800 m: ");
                     int field = scan.nextInt();
 
-                    System.out.println("Enter a performance value: ");
-
                     Performance value = new Performance();
                     switch(field){
 
                         case 1:
-
-                            double HurdlesValue = scan.nextDouble();
-                            value.setPerformanceValue(HurdlesValue);
-                            System.out.println("The saved value for 100 m Hurdles is " + HurdlesValue);
+                            System.out.println("Enter a performance value in seconds: ");
+                            double hurdlesValue = scan.nextDouble();
+                            value.setPerformanceValue(hurdlesValue);
+                            System.out.println("The saved value for 100 m Hurdles is " + hurdlesValue + "s");
                             System.out.println("The score is " +  value.computeScoreHepta(field));
                             break;
 
                         case 2:
+                            System.out.println("Enter a performance value in centimeters: ");
+                            double highJumpValue = scan.nextDouble();
+                            value.setPerformanceValue(highJumpValue);
+                            System.out.println("The saved value for High Jump is " + highJumpValue + "cm");
+                            System.out.println("The score is " +  value.computeScoreHepta(field));
+                            break;
 
-                            double HighJumpValue = scan.nextDouble();
-                            value.setPerformanceValue(HighJumpValue);
-                            System.out.println("The saved value for High Jump is " + HighJumpValue);
+                        case 3:
+                            System.out.println("Enter a performance value in meters: ");
+                            double shotPut = scan.nextDouble();
+                            value.setPerformanceValue(shotPut);
+                            System.out.println("The saved value for Shot put is " + shotPut + "m");
                             System.out.println("The score is " +  value.computeScoreHepta(field));
                             break;
                     }
--- src/main/java/allRoundScoring/Performance.java
+++ src/main/java/allRoundScoring/Performance.java
@@ -21,10 +21,13 @@ public class Performance {
         switch(input){
             case 1:
                 double helpScore= Math.pow((18- performanceValue),1.81);
-                score= (int) (25.4347*helpScore);
+                score+= (int) (25.4347*helpScore);
                 break;
 
             case 2:
+                double longJump= Math.pow((performanceValue- 220),1.4);
+                score+= (int) (0.14354*longJump);
+                break;
 
         }
         return score;
@@ -36,12 +39,17 @@ public class Performance {
         switch(input){
             case 1:
                 double hurdles= Math.pow((26.7- performanceValue),1.835);
-                score= (int) (9.23076 *hurdles);
+                score+= (int) (9.23076 *hurdles);
                 break;
 
             case 2:
                 double highJump= Math.pow((performanceValue- 75),1.348);
-                score= (int) (1.84523 *highJump);
+                score+= (int) (1.84523 *highJump);
+                break;
+
+            case 3:
+                double shotPut= Math.pow((performanceValue- 1.5),1.05);
+                score+= (int) (51.39 *shotPut);
                 break;
 
         }
