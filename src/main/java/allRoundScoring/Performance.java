5656e00af7b9469f5ae32365393a18ed587492c9
Sudden123 <emil.sundius@gmail.com>
2021-05-18 10:25:04 +0200

    Most recent

--- src/main/java/allRoundScoring/Performance.java
+++ src/main/java/allRoundScoring/Performance.java
@@ -2,7 +2,7 @@ package allRoundScoring;
 
 public class Performance {
 
-    private double performanceValue=0;
+    private double performanceValue = 0;
 
 
     public void setPerformanceValue(double input) {
@@ -15,99 +15,100 @@ public class Performance {
         return performanceValue;
     }
 
-    public int computeScore(int input){
+    public int computeScore(int input) {
 
-        int score= 0;
-        switch(input){
+        int score = 0;
+        switch (input) {
             case 1:
-                double m100= Math.pow((18- performanceValue),1.81);
-                score+= (int) (25.4347*m100);
+                double m100 = Math.pow((18 - performanceValue), 1.81);
+                score += (int) (25.4347 * m100);
                 break;
 
             case 2:
-                double longJump= Math.pow((performanceValue- 220),1.4);
-                score+= (int) (0.14354*longJump);
+                double longJump = Math.pow((performanceValue - 220), 1.4);
+                score += (int) (0.14354 * longJump);
                 break;
             case 3:
-                double shotPut= Math.pow((performanceValue- 1.5),1.05);
-                score+= (int) (51.39 *shotPut);
+                double shotPut = Math.pow((performanceValue - 1.5), 1.05);
+                score += (int) (51.39 * shotPut);
                 break;
             case 4:
-                double highJump= Math.pow((performanceValue- 75),1.42);
-                score+= (int) (0.8465 *highJump);
-                break;
-
-            case 8:
-                double poleVault= Math.pow((performanceValue-100),1.35);
-                score= (int) (0.2797*poleVault);
-                break;
-
-            case 9:
-                double javelinThrow= Math.pow((performanceValue-7),1.08);
-                score= (int) (10.14*javelinThrow);
-                break;
-
-            case 10:
-                double m1500= Math.pow((480-performanceValue),1.85);
-                score= (int) (0.03768*m1500);
+                double highJump = Math.pow((performanceValue - 75), 1.42);
+                score += (int) (0.8465 * highJump);
                 break;
 
             case 5:
-                double m400= Math.pow((82-performanceValue),1.81);
-                score= (int) (1.53775*m400);
+                double m400 = Math.pow((82 - performanceValue), 1.81);
+                score = (int) (1.53775 * m400);
                 break;
 
             case 6:
-                double hurdl= Math.pow((28.5-performanceValue),1.92);
-                score= (int) (5.74352*hurdl);
+                double hurdl = Math.pow((28.5 - performanceValue), 1.92);
+                score = (int) (5.74352 * hurdl);
                 break;
 
             case 7:
-                double discusThrow= Math.pow((performanceValue-4),1.1);
-                score= (int) (12.91*discusThrow);
+                double discusThrow = Math.pow((performanceValue - 4), 1.1);
+                score = (int) (12.91 * discusThrow);
                 break;
+
+            case 8:
+                double poleVault = Math.pow((performanceValue - 100), 1.35);
+                score = (int) (0.2797 * poleVault);
+                break;
+
+            case 9:
+                double javelinThrow = Math.pow((performanceValue - 7), 1.08);
+                score = (int) (10.14 * javelinThrow);
+                break;
+
+            case 10:
+                double m1500 = Math.pow((480 - performanceValue), 1.85);
+                score = (int) (0.03768 * m1500);
+                break;
+
 
         }
         return score;
     }
 
-    public int computeScoreHepta(int input){
+    public int computeScoreHepta(int input) {
 
-        int score= 0;
-        switch(input){
+        int score = 0;
+        switch (input) {
             case 1:
-                double hurdles= Math.pow((26.7- performanceValue),1.835);
-                score+= (int) (9.23076 *hurdles);
+                double hurdles = Math.pow((26.7 - performanceValue), 1.835);
+                score += (int) (9.23076 * hurdles);
                 break;
 
             case 2:
-                double highJump= Math.pow((performanceValue- 75),1.348);
-                score+= (int) (1.84523 *highJump);
+                double highJump = Math.pow((performanceValue - 75), 1.348);
+                score += (int) (1.84523 * highJump);
                 break;
 
             case 3:
-                double shotPut= Math.pow((performanceValue- 1.5),1.05);
-                score+= (int) (56.0211 *shotPut);
+                double shotPut = Math.pow((performanceValue - 1.5), 1.05);
+                score += (int) (56.0211 * shotPut);
                 break;
 
             case 4:
-                double twoHundredMeters= Math.pow((42.5- performanceValue),1.81);
-                score+= (int) (4.99087 *twoHundredMeters);
+                double twoHundredMeters = Math.pow((42.5 - performanceValue), 1.81);
+                score += (int) (4.99087 * twoHundredMeters);
                 break;
 
             case 5:
-                double longJump= Math.pow((performanceValue- 210),1.41);
-                score+= (int) (0.188807 *longJump);
+                double longJump = Math.pow((performanceValue - 210), 1.41);
+                score += (int) (0.188807 * longJump);
                 break;
 
             case 6:
-                double javelinThrow= Math.pow((performanceValue- 3.8),1.04);
-                score+= (int) (15.9803 *javelinThrow);
+                double javelinThrow = Math.pow((performanceValue - 3.8), 1.04);
+                score += (int) (15.9803 * javelinThrow);
                 break;
 
             case 7:
-                double eightHundredMeters= Math.pow((254- performanceValue),1.88);
-                score+= (int) (0.11193 *eightHundredMeters);
+                double eightHundredMeters = Math.pow((254 - performanceValue), 1.88);
+                score += (int) (0.11193 * eightHundredMeters);
                 break;
 
         }
