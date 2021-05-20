a6e9057bffca3d40e9063d5ed0e849fd5754279a
Eduardo <eduardo.rene.parra@gmail.com>
2021-05-20 09:29:27 +0200

    back to basic

---
+++ src/main/java/allRoundScoring/Competitor.java
@@ -0,0 +1,58 @@
+package allRoundScoring;
+
+import java.util.Hashtable;
+
+public class Competitor {
+    private String firstName = "";
+    private String lastName = "";
+   /* private Hashtable<String, Integer> scores = new Hashtable<String, Integer>();
+    public Competitor(){
+        scores.put("100m", 0);
+    }
+    public void setScore(String event, Integer score) {
+        scores.put(event, score);
+    }
+    public int getScore(String event) {
+        return scores.get(event);
+    }*/
+
+    private static boolean isValidName(String name){
+        return name.matches("(?i)[a-z]([- ',.a-z]{0,23}[a-z])?");
+    }
+
+    public void setFirstName(String name) {
+
+        firstName = name;
+    }
+
+    public void setLastName(String name){
+        lastName = name;
+    }
+
+
+    public String getFirstName(String name) {
+        try {
+            while (true) {
+
+                if (isValidName(name) == true) {
+                    System.out.println("Valid name");
+                    break;
+                }
+            }
+        }catch (Exception e){
+            System.out.println("Invalid name");
+        }
+        return firstName;
+    }
+
+    public String getLastName(String name) {
+        if (isValidName(name)==true){
+            System.out.println("Valid name");
+        }else {
+            System.out.println("Invalid name");
+        }
+        return lastName;
+    }
+
+
+}
