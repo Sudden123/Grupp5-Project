3c77952a9a2201f22f4b6ea220ad0b39ef631f0c
Lajos2x <lajos.zsoka1@gmail.com>
2021-05-21 22:56:51 +0200

    Excel jobb Lajos and Eduardo

--- src/main/java/allRoundScoring/Excel.java
+++ src/main/java/allRoundScoring/Excel.java
@@ -13,14 +13,21 @@ public class Excel {
     public static void main(String[] args) throws IOException {
 
         XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet0= workbook.createSheet("firstSheet");
+        XSSFSheet sheet0= workbook.createSheet("Decathlon");
+        XSSFSheet sheet1= workbook.createSheet("Heptathlon");
         Row row0 = sheet0.createRow(0);
+        Row row1 = sheet0.createRow(1);
 
         Cell cellA = row0.createCell(0);
         Cell cellB = row0.createCell(1);
-
-        cellA.setCellValue("first cell");
-        cellB.setCellValue("second cell");
+        Cell cellC = row0.createCell(2);
+        Cell cellD = row0.createCell(3);
+        Cell cellE = row1.createCell(0);
+        cellA.setCellValue("Name");
+        cellB.setCellValue("Spjutkastning");
+        cellC.setCellValue("diskus");
+        cellD.setCellValue("hundra meter");
+        cellE.setCellValue("Daniel");
 
         File f = new File("C:\\Prov\\Grupp5.xlsx");
         FileOutputStream fo = new FileOutputStream(f);
